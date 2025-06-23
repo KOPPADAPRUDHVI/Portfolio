fa540e1cb8f43d2a8dc1de5ff33550ccd72cf3ae
Prudhvi-Vinayak <koppadaprudhvi3@gmail.com>
2025-06-23 23:05:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AnimatedBackground from "./components/Background";
+import Navbar from "./components/Navbar";
+import ProjectDetails from "./components/ProjectDetail";
+import "./index.css";
+import NotFoundPage from "./Pages/404";
+import About from "./Pages/About";
+import ContactPage from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Portofolio from "./Pages/Portofolio";
+import WelcomeScreen from "./Pages/WelcomeScreen";
+
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <About />
+          <Portofolio />
+          <ContactPage />
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © 2025{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                  Prudhvi™
+                </a>
+                . All Rights Reserved.
+              </span>
+            </center>
+          </footer>
+        </>
+      )}
+    </>
+  );
+};
+
+const ProjectPageLayout = () => (
+  <>
+    <ProjectDetails />
+    <footer>
+      <center>
+        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          © 2025{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Prudhvi™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </center>
+    </footer>
+  </>
+);
+
+function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
+        <Route path="/project/:id" element={<ProjectPageLayout />} />
+         <Route path="*" element={<NotFoundPage />} /> {/* Ini route 404 */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
